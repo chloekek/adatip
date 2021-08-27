c9e58f73bc24a16c7915fbccdfce73179e8b6505
Chloé <rightfold+ck@gmail.com>
2021-08-27 12:52:37 +0200

    fixup! Add Procfile and Nginx configuration

--- scripts/run-nginx.bash
+++ scripts/run-nginx.bash
@@ -4,12 +4,6 @@
 
 set -efuo pipefail
 
-# Ok so for relative paths, Nginx uses the prefix path as prefix.
-# But we want to have a path relative to the working directory.
-# But inside the Nginx configuration, you can’t retrieve the working directory.
-# So we generate a file that contains it absolute, and include it in nginx.conf.
-echo "alias $PWD/adatipd/static/;" > state/nginx/alias_static.conf;
-
 # -c: Path to configuration file.
 # -p: Prefix path, used for state.
 exec nginx -c "$PWD"/scripts/nginx.conf -p state/nginx
