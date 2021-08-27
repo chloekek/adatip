5d6ebaa2723059d4f362c748b2c2570ca53839d1
Chloé Kekoa <chloekek@use.startmail.com>
2021-08-28 00:06:05 +0200

    fixup! Add database schema migrations

--- scripts/dbmate.bash
+++ scripts/dbmate.bash
@@ -5,6 +5,8 @@
 # PostgreSQL must already be running.
 # [1]: https://github.com/amacneil/dbmate
 
+set -efuo pipefail
+
 pg_url='postgres://adatip_setup:adatip_setup@127.0.0.1:8082/adatip?sslmode=disable'
 
 exec dbmate                   \
