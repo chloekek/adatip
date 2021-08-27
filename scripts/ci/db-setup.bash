6126c124ae62aa1a46b9fba35761dd02629943af
Chloé Kekoa <chloekek@use.startmail.com>
2021-08-28 00:28:17 +0200

    Check that database setup works in CI

---
+++ scripts/ci/db-setup.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# This script is used by GitHub actions.
+
+set -efuo pipefail
+
+# Kill postgres when script exits.
+trap 'kill $(jobs -p)' EXIT
+
+# Start postgres in the background.
+scripts/run-postgres.bash &
+
+# Wait for postgres to be running.
+for (( i = 0; i < 10; ++i )); do
+    if pg_isready -h 127.0.0.1 -p 8082; then
+        break
+    else
+        sleep 1
+    fi
+done
+
+# These scripts should now all succeed.
+scripts/setup-database.bash
+scripts/dbmate.bash migrate
+scripts/seed-database.bash
